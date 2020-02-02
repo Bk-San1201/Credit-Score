f98e3b8bb74b91c768b6d13ca4f2377a8315089d
Dang Lam San 20170111 <san.dl170111@sis.hust.edu.vn>
2020-02-02 15:40:05 +0700

    sth

--- src/pre_process/pre.py
+++ src/pre_process/pre.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pandas import DataFrame
 import csv
-
+import json
+import numpy as np
 def format():   
     with open('./Data/quan_huyen.txt', encoding='utf8') as file:
         prvs = file.read().splitlines()
@@ -28,12 +29,17 @@ def remove_accents(input_str):
     s0 = u'AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy'
     s = ''
     # print(input_str.encode('utf-8'))
-    for c in input_str:
-        if c in s1:
-            s += s0[s1.index(c)]
-        else:
-            s += c
-    return s.lower()      
+    try:
+        for c in input_str:
+            if c in s1:
+                s += s0[s1.index(c)]
+            else:
+                s += c
+        return s.lower()
+    except:
+        return None
+
+         
     
 def make_train_output():
     df = pd.read_csv('./Data/train.csv')
@@ -43,8 +49,49 @@ def make_train_output():
 
 def make_train_input():
     df = pd.read_csv('./Data/train.csv')
-    df.pop('label')
+    # df.pop('label')
+    # df.to_csv('./Data/train_input.csv', index=False)
+
+    ar = []
+    prvs = df['province'].values.tolist()
+    for prv in prvs:
+        # print(prv)
+        ar.append(remove_accents(prv))
+    npAr = np.array(ar)
+    df['tinh'] = npAr
     df.to_csv('./Data/train_input.csv', index=False)
 
+    ar1 = []
+    prvs = df['district'].values.tolist()
+    for prv in prvs:
+        # print(prv)
+        ar1.append(remove_accents(prv))
+    npAr = np.array(ar1)
+    print(len(npAr))
+    df['huyen'] = npAr
+    df.to_csv('./Data/train_input.csv', index=False)
+
+
+
+def check_field_7():
+    df = pd.read_csv('./Data/train.csv')
+    col = DataFrame(df, columns = ['FIELD_7']).values.tolist()
+    for row in col:
+        # res = json.loads(row)
+        # print(res)
+        print(row)
+
+def test():
+    df = pd.read_csv('./Data/train_input.csv', low_memory=False)
+    df_huyen = pd.read_csv('./Data/huyen.csv', low_memory=False)
+
+    listDis = df_huyen['huyen'].values.tolist()
+    tmp = df['huyen'].unique()
+    i = 0
+    for dis in tmp:
+        if listDis.count(dis) == 0:
+            print(dis)
+            i += 1
+    print(i / len(tmp))
 if __name__ == "__main__":
-    format()
+    test()
